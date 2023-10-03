59d476b17d7d3c53fbb987fcc71fbea586722863
Annie Wu <79778718+awutis@users.noreply.github.com>
2023-10-03 13:16:15 -0400

    Add files via upload

---
+++ PriceCheckFB_starter-main/PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -0,0 +1,41 @@
+// Created by Prof. H in 2022
+// Part of the PriceCheckFB project
+// Using Swift 5.0
+// Qapla'
+
+
+import Combine
+// import Firebase modules here
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+
+
+class LocationRepository: ObservableObject {
+    // Set up properties here
+    private let path: String = "location_scans"
+    private let store = Firestore.firestore()
+    
+    @Published var locations: [Location] = []
+    
+    
+    init() {
+        get()
+    }
+    
+    func get() {
+        // Complete this function
+        store.collection(path)
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    print("Error getting books: \(error.localizedDescription)")
+                    return
+                }
+                
+                self.locations = querySnapshot?.documents.compactMap { document in
+                    try? document.data(as: Location.self)
+                } ?? []
+                
+            }
+    }}
